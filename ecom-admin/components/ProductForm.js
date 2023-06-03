0d7291fb81ace43daeba3c5d901404e53e0e2c5c
elkhellafanass <elkhellafanass@gmail.com>
2023-06-03 16:40:18 +0100

    update.v2

---
+++ ecom-admin/components/ProductForm.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { data } from "autoprefixer";
+
+
+export default function ProductForm({
+    _id,
+    title:existingTitle,
+    description:existingDescription,
+    price:existingPrice,
+}) {
+    const [title,setTitle] = useState(existingTitle || '');
+    const [description,setDescription] = useState(existingDescription || '');
+    const [price,setPrice] = useState(existingPrice || '');
+    const [goToProducts,setGoToProducts] = useState(false);
+    const router = useRouter();
+    async function saveProduct(ev) {
+        ev.preventDefault();
+        const data = {title,description,price};
+        if (_id) {
+            //update
+            await axios.put('/api/products', {...data,_id});
+        } else {
+            //create
+            await axios.post('/api/products', data);
+        }
+        setGoToProducts(true);
+
+    }
+    if (goToProducts) {
+        router.push('/products');
+    }
+
+    return(
+            <form onSubmit={saveProduct}>
+
+                <label>Product Name</label>
+                <input type="text" placeholder="Product Name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+
+                <label>Description</label>
+                <textarea placeholder="Description" value={description} onChange={ev => setDescription(ev.target.value)}/>
+
+                <label>Price (In MAD)</label>
+                <input type="number" placeholder="Price" value={price} onChange={ev => setPrice(ev.target.value)}/>
+
+                <button type="submit" className="btn-primary">Save</button>
+
+            </form>
+    );
+}
