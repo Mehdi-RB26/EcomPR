5a53dab543732da461b814df1602bc8791e28b37
Mehdi-RB26 <76706126+Mehdi-RB26@users.noreply.github.com>
2023-06-08 13:20:22 +0100

    upload
    Working on upload/image

---
+++ ecom-admin/pages/api/upload..js
@@ -0,0 +1,49 @@
+import multiparty from 'multiparty';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+const bucketName = 'skyline-eco';
+
+export default async function handle(req,res)
+{
+    const form = new multiparty.Form();
+    const {fields,files} = await new Promise((resolve,reject) => {
+
+    form.parse(req, (err, fields, files) =>    
+     {
+        if (err) reject(err);
+        resolve({fields,files});
+
+        
+       
+     });
+    });
+     console.log('length:', files.file.length);
+     const client = new S3Client({
+      region:'eu-west-3',
+      credentials:
+      {
+          accessKeyId : process.env.S3_ACCESS_KEY,
+          secretAccessKey : process.env.S3_SECRET_ACCESS_KEY,
+      },
+   });
+      for (const file of files.file)
+      {
+         const ext = file.originalFilename.split('.').pop();
+         const newFilename = Date.now()+ '.' + ext;
+         console.log({ext,file});
+
+         await client.send(new PutObjectCommand({
+            Bucket : bucketName,
+            Key:newFilename,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: ''
+    
+         }));
+      }     
+     return res.json('ok');
+}
+
+export const config = {
+    api: {bodyParser: false},
+};
