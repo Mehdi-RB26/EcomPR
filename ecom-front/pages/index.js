b2a43be40f117fe6ae98b86938f10629c95f1144
Mehdi-RB26 <76706126+Mehdi-RB26@users.noreply.github.com>
2023-06-27 16:08:56 +0100

    NewProducts Update

--- ecom-front/pages/index.js
+++ ecom-front/pages/index.js
@@ -1,14 +1,16 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import NewProducts from "@/components/NewPoducts";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/product";
 
-export default function HomePage({product}) {
-  
+export default function HomePage({featuredProduct,newProducts}) {
+  console.log({newProducts});
   return (
    <div>
     <Header />
-    <Featured product={product}/> 
+    <Featured product={featuredProduct}/> 
+    <NewProducts products={newProducts}/>
    </div>
  );
 }
@@ -17,9 +19,11 @@ export default function HomePage({product}) {
 export async function getServerSideProps() {
   const featuredProductId = '648b30fbb165ef418a51ef1d';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
-  //const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {featuredProduct: JSON.parse(JSON.stringify(product)),
+    newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
